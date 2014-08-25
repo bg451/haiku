1aeafe9a3d4a05090c64c3ed56befcacfc6985b9
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2014-08-24 18:42:19 -0700

    Changed a lot of stuff idk

--- web.go
+++ web.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 
 	"github.com/gorilla/mux"
@@ -10,34 +12,97 @@ import (
 
 const httpPort = 8080
 
-var db *Database
+var database *Database = new(Database)
 
-type Post struct {
-	Swag string
-}
+type httpApiFunc func(http.ResponseWriter, *http.Request)
 
 func main() {
-	database = new(Database)
-	database.db = initDb("./db.sqlite3")
-	go webServer()
+	database = initDb("./foo.db")
+	getAll()
+	startServer()
 	for {
+
 	}
 }
-func webServer() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/", indexPageHandler).Methods("GET")
-	r.HandleFunc("/{blog}", indexPageHandler).Methods("GET")
-	http.Handle("/static/", http.StripPrefix("/static/",
-		http.FileServer(http.Dir("static/"))))
-	http.Handle("/", r)
+func addHandlers(router *mux.Router) {
+	m := map[string]map[string]httpApiFunc{
+		"GET": {
+			"/":                    getIndexPage,
+			"/videos":              getVideos,
+			"/videos/:id":          getVideosID,
+			"/videos/elo/highest":  getVideosEloHighest,
+			"/matches":             getMatches,
+			"/matches/new":         getMatchesNew,
+			"/matches/{id:[0-9]+}": getMatchesID,
+		},
+		"POST": {
+			"/videos/new":    postVideosNew,
+			"matches/result": postMatchesResult,
+		},
+	}
+	for method, routes := range m {
+		for route, fnc := range routes {
+			router.HandleFunc(route, fnc).Methods(method)
+		}
+	}
+}
+func startServer() {
+	router := mux.NewRouter()
+	addHandlers(router)
+	http.Handle("/", router)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	fmt.Printf("Serving http server")
 	http.ListenAndServe(":8080", nil)
 }
-func indexPageHandler(w http.ResponseWriter, r *http.Request) {
+func getIndexPage(w http.ResponseWriter, r *http.Request) {
 	templ, err := template.ParseFiles("templates/blog.html", "templates/base.html")
 	if err != nil {
 		fmt.Fprintf(w, "There was a an error %s", err)
 		return
 	}
 	templ.ExecuteTemplate(w, "base", nil)
+}
+func getMatches(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "getting matches")
+}
+func getMatchesNew(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "getting matches/new")
+
+}
+func getMatchesID(w http.ResponseWriter, r *http.Request) {
+	log.Printf("asfasfasd")
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 0, 0)
+	if err != nil {
+		fmt.Fprintf(w, "Could not parse int")
+		return
+	}
+	swag := int(id)
+
+	match, err := findMatchById(swag)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %q", err)
+		return
+	}
+	fmt.Fprintf(w, "The winner is %t", match.winnerA)
+}
+func getVideos(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "getting videos")
+
+}
+func getVideosID(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "getting videos/:id")
+
+}
+func getVideosEloHighest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "getting videos/elo/highest")
+
+}
+
+func postVideosNew(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func postMatchesResult(w http.ResponseWriter, r *http.Request) {
+
 }
